4d5db8bb7a04688c0f20725a041a96a1ea11057a
Nuttachai-Su <lasernaball@gmail.com>
2025-12-19 21:11:21 +0700

    Add files via upload

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 //Write the function printO() here
 
+char printO(int N,int M){
+	int line = 1, NumM = 1;
+	if (N == 0) cout << "Invalid input\n";
+	if (M < 0) cout << "Invalid input\n";
+	while (line <= N){
+		while (NumM <= M){
+			cout << "O";
+			NumM++;
+			}
+		NumM = 1;
+		cout << "\n";
+		line++;
+	}
+	return 0;
+}
+
 int main(){
 	
 	printO(2,2);
@@ -18,7 +34,7 @@ int main(){
 	cout << "\n";	
 	
 	printO(7,-1);
-	cout << "\n";	
+	cout << "\n";
 	
 	return 0;
 }
